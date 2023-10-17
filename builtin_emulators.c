e4e2fd3830fa2cd87944faeff7897cc68809e248
Fagr Elaskary <48035751+fagrahmed12@users.noreply.github.com>
2023-10-17 12:36:34 +0300

    Update builtin_emulators.c

--- builtin_emulators.c
+++ builtin_emulators.c
@@ -1,23 +1,16 @@
 #include "shell.h"
 
-/**
- * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: exits with a given exit status
- *         (0) if info.argv[0] != "exit"
- */
 int _myexit(info_t *info)
 {
-	int exitcheck;
+	int e_check;
 
-	if (info->argv[1])  /* If there is an exit arguement */
+	if (info->argv[1])  
 	{
-		exitcheck = _erratoi(info->argv[1]);
-		if (exitcheck == -1)
+		e_check= _erratoi(info->argv[1]);
+		if (e_check == -1)
 		{
 			info->status = 2;
-			print_error(info, "Illegal number: ");
+			print_error(info, "Illegal Number: ");
 			_eputs(info->argv[1]);
 			_eputchar('\n');
 			return (1);
@@ -29,19 +22,14 @@ int _myexit(info_t *info)
 	return (-2);
 }
 
-/**
- * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
- */
+
 int _mycd(info_t *info)
 {
-	char *s, *dir, buffer[1024];
+	char *x, *dir, buffer[1024];
 	int chdir_ret;
 
-	s = getcwd(buffer, 1024);
-	if (!s)
+	x = getcwd(buffer, 1024);
+	if (!x)
 		_puts("TODO: >>getcwd failure emsg here<<\n");
 	if (!info->argv[1])
 	{
@@ -56,7 +44,7 @@ int _mycd(info_t *info)
 	{
 		if (!_getenv(info, "OLDPWD="))
 		{
-			_puts(s);
+			_puts(x);
 			_putchar('\n');
 			return (1);
 		}
@@ -79,19 +67,13 @@ int _mycd(info_t *info)
 	return (0);
 }
 
-/**
- * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
- */
 int _myhelp(info_t *info)
 {
 	char **arg_array;
 
 	arg_array = info->argv;
-	_puts("help call works. Function not yet implemented \n");
+	_puts("Hellllp call works.Function not yet implemented \n");
 	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
+		_puts(*arg_array); 
 	return (0);
 }
