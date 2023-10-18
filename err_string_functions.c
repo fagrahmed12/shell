1dd614c68fbbde46ef1e19f55a0db2ff2e642e7d
Fagr Elaskary <48035751+fagrahmed12@users.noreply.github.com>
2023-10-18 21:55:07 +0300

    Update err_string_functions.c

--- err_string_functions.c
+++ err_string_functions.c
@@ -9,12 +9,16 @@
 void _eputs(char *str)
 {
 	int i = 0;
-
+ // Check if the input string is NULL
 	if (!str)
 		return;
+	
+    // Iterate through the characters in the string until a null terminator '\0' is reached.
+
 	while (str[i] != '\0')
-	{
+	{       // Call a function "_eputchar" to output each character one by one.
 		_eputchar(str[i]);
+		// Increment the index to move to the next character in the string.
 		i++;
 	}
 }
@@ -22,22 +26,25 @@ void _eputs(char *str)
 /**
  * _eputchar - writes the character c to stderr
  * @c: The character to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _eputchar(char c)
-{
-	static int i;
+{ // Check if the character should trigger a buffer flush or if the buffer is full.
+	static int a;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(2, buf, i);
-		i = 0;
+	if (c == BUF_FLUSH || a >= WRITE_BUF_SIZE)
+	{   // If a flush is triggered or the buffer is full, write the contents of the buffer to file descriptor 2 (typically stderr).
+		write(2, buf, a);
+		// Reset the buffer index 'i' to 0 for the next set of characters.
+		a = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+	// If the character is not a flush trigger, store it in the buffer and increment the buffer index.
+        if (c != BUF_FLUSH)
+	
+		buf[a++] = c;
+	// Return 1 to indicate that the character has been successfully processed.
 	return (1);
 }
 
@@ -45,22 +52,23 @@ int _eputchar(char c)
  * _putfd - writes the character c to given fd
  * @c: The character to print
  * @fd: The filedescriptor to write to
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putfd(char c, int fd)
 {
-	static int i;
+	static int a;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(fd, buf, i);
-		i = 0;
+    // Check if the character should trigger a buffer flush or if the buffer is full.
+	if (c == BUF_FLUSH || a >= WRITE_BUF_SIZE)
+	{       // If a flush is triggered or the buffer is full, write the contents of the buffer to the specified file descriptor 'fd'.
+		write(fd, buf, a);
+		// Reset the buffer index 'a' to 0 for the next set of characters.
+		a = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buf[a++] = c;
 	return (1);
 }
 
@@ -73,13 +81,15 @@ int _putfd(char c, int fd)
  */
 int _putsfd(char *str, int fd)
 {
-	int i = 0;
-
+	int x = 0;
+// Check if the input string is NULL.
 	if (!str)
 		return (0);
+	
+    // Iterate through the characters in the string until a null terminator '\0' is reached.
 	while (*str)
 	{
-		i += _putfd(*str++, fd);
+		x += _putfd(*str++, fd);
 	}
-	return (i);
+	return (x);
 }
