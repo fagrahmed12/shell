f757628a34e6c3d6169ec465dd729e32b00f0a2c
Fagr Elaskary <48035751+fagrahmed12@users.noreply.github.com>
2023-10-18 21:31:30 +0300

    Update env2.c

--- env2.c
+++ env2.c
@@ -2,32 +2,30 @@
 
 /**
  * get_environ - returns the string array copy of our environ
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * @info: Structure containing potential arguments. 
  * Return: Always 0
  */
-char **get_environ(info_t *info)
+char **get_environ(info_t *inf)
 {
-	if (!info->environ || info->env_changed)
+	if (!inf->environ || inf->env_changed)
 	{
-		info->environ = list_to_strings(info->env);
-		info->env_changed = 0;
+		inf->environ = list_to_strings(inf->env);
+		inf->env_changed = 0;
 	}
 
-	return (info->environ);
+	return (inf->environ);
 }
 
 /**
  * _unsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: 1 on delete, 0 otherwise
+ * @info: Structure containing potential arguments. 
+ *  Return: 1 on delete,otherwise 0
  * @var: the string env var property
  */
 int _unsetenv(info_t *info, char *var)
 {
 	list_t *node = info->env;
-	size_t i = 0;
+	size_t x = 0;
 	char *p;
 
 	if (!node || !var)
@@ -38,22 +36,20 @@ int _unsetenv(info_t *info, char *var)
 		p = starts_with(node->str, var);
 		if (p && *p == '=')
 		{
-			info->env_changed = delete_node_at_index(&(info->env), i);
-			i = 0;
+			info->env_changed = delete_node_at_index(&(info->env), x);
+			x = 0;
 			node = info->env;
 			continue;
 		}
 		node = node->next;
-		i++;
+		x++;
 	}
 	return (info->env_changed);
 }
 
 /**
- * _setenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ * _setenv - Initialize a new environment variable.
+ * @info: Structure containing potential arguments. 
  * @var: the string env var property
  * @value: the string env var value
  *  Return: Always 0
