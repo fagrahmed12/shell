875bf476c6c4a3a6a18d42b95dd03492ef4fe7a6
Fagr Elaskary <48035751+fagrahmed12@users.noreply.github.com>
2023-10-18 22:46:08 +0300

    Update file_io_functions.c

--- file_io_functions.c
+++ file_io_functions.c
@@ -3,23 +3,31 @@
 /**
  * get_history_file - gets the history file
  * @info: parameter struct
- *
  * Return: allocated string containg history file
  */
 
 char *get_history_file(info_t *info)
 {
 	char *buf, *dir;
-
+// Obtain the user's home directory using the _getenv function.
 	dir = _getenv(info, "HOME=");
+	
+    // Check if the "HOME" environment variable exists. If not, return NULL.
 	if (!dir)
 		return (NULL);
+	// Allocate memory for a buffer that will hold the path to the history file.
+    // The size of the buffer is calculated as the sum of the lengths of 'dir' and 'HIST_FILE' plus 2 
 	buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
+	// Check if memory allocation was successful. If not, return NULL.
 	if (!buf)
 		return (NULL);
+	 // Initialize the buffer with a null-terminator.
 	buf[0] = 0;
+	// Copy the 'dir' string to the buffer using _strcpy.
 	_strcpy(buf, dir);
+	// Concatenate a '/' character to the buffer using _strcat.
 	_strcat(buf, "/");
+	// Concatenate the 'HIST_FILE' string to the buffer using _strcat.
 	_strcat(buf, HIST_FILE);
 	return (buf);
 }
@@ -27,27 +35,32 @@ char *get_history_file(info_t *info)
 /**
  * write_history - creates a file, or appends to an existing file
  * @info: the parameter struct
- *
  * Return: 1 on success, else -1
  */
 int write_history(info_t *info)
 {
 	ssize_t fd;
-	char *filename = get_history_file(info);
+	char *f_name = get_history_file(info);
 	list_t *node = NULL;
-
-	if (!filename)
+// Check if the history file name is valid (not NULL).
+	if (!f_name)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
-	free(filename);
+// Attempt to open the history file for writing. The file is created if it doesn't exist, and its content is truncated.
+	fd = open(f_name, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	// Free the memory allocated for the history file name.
+	free(f_name);
+	// If opening the file fails (fd == -1), return an error code.
 	if (fd == -1)
 		return (-1);
+	// Iterate through the list of history commands stored in info->history.
 	for (node = info->history; node; node = node->next)
 	{
+		// Write the command string (node->str) to the file descriptor 'fd' using _putsfd.
 		_putsfd(node->str, fd);
+		// Append a newline character to separate commands within the file.
 		_putfd('\n', fd);
 	}
+	// Flush the buffer by sending BUF_FLUSH to the file descriptor 'fd'.
 	_putfd(BUF_FLUSH, fd);
 	close(fd);
 	return (1);
@@ -61,7 +74,7 @@ int write_history(info_t *info)
  */
 int read_history(info_t *info)
 {
-	int i, last = 0, linecount = 0;
+	int x, last = 0, linecount = 0;
 	ssize_t fd, rdlen, fsize = 0;
 	struct stat st;
 	char *buf = NULL, *filename = get_history_file(info);
@@ -85,14 +98,14 @@ int read_history(info_t *info)
 	if (rdlen <= 0)
 		return (free(buf), 0);
 	close(fd);
-	for (i = 0; i < fsize; i++)
-		if (buf[i] == '\n')
+	for (x = 0; x < fsize; x++)
+		if (buf[x] == '\n')
 		{
-			buf[i] = 0;
+			buf[x] = 0;
 			build_history_list(info, buf + last, linecount++);
-			last = i + 1;
+			last = x + 1;
 		}
-	if (last != i)
+	if (last != x)
 		build_history_list(info, buf + last, linecount++);
 	free(buf);
 	info->histcount = linecount;
@@ -112,14 +125,14 @@ int read_history(info_t *info)
  */
 int build_history_list(info_t *info, char *buf, int linecount)
 {
-	list_t *node = NULL;
+	list_t *Node = NULL;
 
 	if (info->history)
-		node = info->history;
-	add_node_end(&node, buf, linecount);
+		Node = info->history;
+	add_node_end(&Node, buf, linecount);
 
 	if (!info->history)
-		info->history = node;
+		info->history = Node;
 	return (0);
 }
 
@@ -132,12 +145,17 @@ int build_history_list(info_t *info, char *buf, int linecount)
 int renumber_history(info_t *info)
 {
 	list_t *node = info->history;
-	int i = 0;
-
+	int z = 0;
+// Initialize a pointer 'node' to the head of the history linked list and a counter 'i' to 0.
 	while (node)
 	{
-		node->num = i++;
+		// Iterate through the linked list until 'node' is NULL (end of the list).
+
+        // Assign the value of 'i' to the 'num' member of the current 'node'.
+		node->num = z++;
+		// Move to the next node in the list.
 		node = node->next;
 	}
-	return (info->histcount = i);
+	// Return the final value of 'i' as the updated 'info->histcount'.
+	return (info->histcount = z);
 }
